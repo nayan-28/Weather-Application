99629ac620af3595a8c10d59a2e703a5238a9a97
nayan-28 <nayanmalakar720@gmail.com>
2023-12-29 01:33:03 +0600

    responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import WeatherApp from "./Components/WeatherApp/WeatherApp.jsx"
-
+import './App.css'
 function App() {
   return (
     <>
       <div className='App'>
+      <h3 className='blinking-text'>Weather Application</h3>
         <h3><WeatherApp/></h3>
+        <footer className='footer'>
+        <p>&copy; 2023 Nayan Malaakr. All rights reserved.</p>
+      </footer>
       </div>
-
     </>
   )
 }
